9f7619b200b3b1763b30257a8f108607c0f58b87
nurbergenovv <ibrahimffyt@gmail.com>
2023-12-16 05:05:00 +0600

    read_status

--- public/message.post.php
+++ public/message.post.php
@@ -5,11 +5,9 @@ header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
 require_once "connecting.method.php";
 
-// Получение данных из тела запроса
 $request_body = file_get_contents("php://input");
 $data = json_decode($request_body);
 
-// Проверка наличия необходимых данных
 $sender = isset($data->sender) ? $data->sender : '';
 $recipient = isset($data->recipient) ? $data->recipient : '';
 $chat_id = isset($data->chat_id) ? $data->chat_id : '';
@@ -33,11 +31,12 @@ $check_chat_result = $conn->query($check_chat_sql);
 if ($check_chat_result === false) {
     echo json_encode(['status' => 'error', 'message' => 'Database error']);
     exit;
-} else if ($check_chat_result->num_rows === 0) {
+} elseif ($check_chat_result->num_rows === 0) {
     echo json_encode(['status' => 'error', 'message' => 'Chat not found']);
     exit;
-} else if ($check_chat_result->num_rows > 0) {
-    $insert_message_sql = "INSERT INTO messages (sender, recipient, chat_id, date, message) VALUES ('$sender', '$recipient', '$chat_id', '$date', '$message')";
+} else {
+    $insert_message_sql = "INSERT INTO messages (sender, recipient, chat_id, date, message, read_status) VALUES ('$sender', '$recipient', '$chat_id', '$date', '$message', 'unread')";
+
     if ($conn->query($insert_message_sql) === true) {
         echo json_encode(['status' => 'success', 'message' => 'Message sent successfully']);
     } else {
